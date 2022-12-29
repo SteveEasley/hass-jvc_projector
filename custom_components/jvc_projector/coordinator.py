ef8ed7c91e8ac6f5f78594907c703acbcc91878e
Steve Easley <steve.easley@gmail.com>
2022-12-29 14:36:11 -0500

    Fixed bad paste

--- custom_components/jvc_projector/coordinator.py
+++ custom_components/jvc_projector/coordinator.py
@@ -1,77 +1,57 @@
-"""The jvc_projector integration."""
+"""Data update coordinator for the jvc_projector integration."""
 
 from __future__ import annotations
 
+from datetime import timedelta
+import logging
 from typing import TYPE_CHECKING
 
-from jvcprojector import JvcProjector, JvcProjectorAuthError, JvcProjectorConnectError
+from jvcprojector import JvcProjectorAuthError, JvcProjectorConnectError, const
 
-from homeassistant.const import (
-    CONF_HOST,
-    CONF_PASSWORD,
-    CONF_PORT,
-    EVENT_HOMEASSISTANT_STOP,
-    Platform,
-)
-from homeassistant.exceptions import ConfigEntryAuthFailed, ConfigEntryNotReady
+from homeassistant.exceptions import ConfigEntryAuthFailed
+from homeassistant.helpers.device_registry import format_mac
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-from .const import DOMAIN
-from .coordinator import JvcProjectorDataUpdateCoordinator
+from .const import NAME
 
 if TYPE_CHECKING:
-    from homeassistant.config_entries import ConfigEntry
-    from homeassistant.core import Event, HomeAssistant
+    from jvcprojector import JvcProjector
 
-PLATFORMS = [Platform.REMOTE]
+    from homeassistant.core import HomeAssistant
 
+_LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up integration from a config entry."""
-    device = JvcProjector(
-        host=entry.data[CONF_HOST],
-        port=entry.data[CONF_PORT],
-        password=entry.data[CONF_PASSWORD],
-    )
-
-    try:
-        await device.connect(True)
-    except JvcProjectorConnectError as err:
-        raise ConfigEntryNotReady(
-            f"Unable to connect to {entry.data[CONF_HOST]}"
-        ) from err
-    except JvcProjectorAuthError as err:
-        raise ConfigEntryAuthFailed("Password authentication failed") from err
-
-    coordinator = JvcProjectorDataUpdateCoordinator(hass, device)
-    await coordinator.async_config_entry_first_refresh()
-
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
-
-    async def disconnect(event: Event) -> None:
-        await device.disconnect()
-
-    entry.async_on_unload(
-        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, disconnect)
-    )
-
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-
-    return True
+INTERVAL_SLOW = timedelta(seconds=60)
+INTERVAL_FAST = timedelta(seconds=10)
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        await hass.data[DOMAIN][entry.entry_id].device.disconnect()
-        hass.data[DOMAIN].pop(entry.entry_id)
-    return unload_ok
+class JvcProjectorDataUpdateCoordinator(DataUpdateCoordinator):
+    """Data update coordinator for the JVC Projector integration."""
 
+    def __init__(self, hass: HomeAssistant, device: JvcProjector) -> None:
+        """Initialize the coordinator."""
+        super().__init__(
+            hass=hass,
+            logger=_LOGGER,
+            name=NAME,
+            update_interval=INTERVAL_SLOW,
+        )
 
-async def get_mac_address(host: str, port: int, password: str | None) -> str:
-    """Get device mac address for config flow."""
-    device = JvcProjector(host, port=port, password=password)
-    try:
-        await device.connect(True)
-    finally:
-        await device.disconnect()
-    return device.mac
+        self.device = device
+        self.unique_id = format_mac(device.mac)
+
+    async def _async_update_data(self) -> dict[str, str]:
+        """Get the latest state data."""
+        try:
+            state = await self.device.get_state()
+        except JvcProjectorConnectError as err:
+            raise UpdateFailed(f"Unable to connect to {self.device.host}") from err
+        except JvcProjectorAuthError as err:
+            raise ConfigEntryAuthFailed("Password authentication failed") from err
+
+        if state[const.POWER] == const.STANDBY:
+            self.update_interval = INTERVAL_SLOW
+        else:
+            self.update_interval = INTERVAL_FAST
+
+        return state
